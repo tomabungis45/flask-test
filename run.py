752d51496e4d3fbd2261590afdffa934c3ffa9fa
tomabungis45 <85626648+tomabungis45@users.noreply.github.com>
2021-06-10 14:21:27 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from tom import create_app
-from tom import db
+#from tom import db
 
 app = create_app()
-with app.app_context(): db.create_all()
+#with app.app_context(): db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=False)
