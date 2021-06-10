05e358028d188941b69ad72df94aa6abc6cf0114
tomabungis45 <85626648+tomabungis45@users.noreply.github.com>
2021-06-10 14:15:13 +0200

    Update config.py

--- tom/config.py
+++ tom/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = 'YWyLArK5ZWTeKZGg'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'postgres://bsqjhhfdcqqiew:a2c418b00c7169cd4d68d9615fd78f059792103051640709ec723a21aa561dea@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/de0mpkfvg9u3r6k'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
